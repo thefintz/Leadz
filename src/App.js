45b9488aca9ece3152e9a282e83302dd7de71356
Joao Meyer <1994meyer@gmail.com>
2022-03-15 23:46:03 +0000

    Forcing a redeploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchPage from './components/SearchPage.js';
 
 function App() {
   return (
-    <div className="App" style={{"background": "#f6f9fc", "minHeight": "100vh"}}>
+    <div className="App" style={{ "background": "#f6f9fc", "minHeight": "100vh" }}>
       <SearchPage />
     </div>
   );
